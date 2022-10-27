b2af009740e9bca4c427fefb1687e97643a5977d
Helen Diana Formighieri <helendianaf@gmail.com>
2022-10-26 22:02:15 -0300

    Create atividade.py

---
+++ atividade.py
@@ -0,0 +1,26 @@
+#Desenvolva um código que utilize as seguintes características de um veículo:
+#- Quantidade de rodas;
+#- Peso bruto em quilogramas;
+#- Quantidade de pessoas no veículo.
+
+#Com essas informações, o programa mostrará qual é a melhor categoria de habilitação para o veículo informado a partir das condições:
+#A: Veículos com duas ou três rodas;
+#B: Veículos com quatro rodas, que acomodam até oito pessoas e seu peso é de até 3500 kg;
+#C: Veículos com quatro rodas ou mais e com peso entre 3500 e 6000 kg;
+#D: Veículos com quatro rodas ou mais e que acomodam mais de oito pessoas; 
+#E: Veículos com quatro rodas ou mais e com mais de 6000 kg.
+
+rodas = 4
+peso = 0
+pessoas = 9
+
+if (rodas <= 3):
+    print("A")
+elif (rodas == 4) and (pessoas <= 8) and (peso <= 3500): 
+    print("B")
+elif (rodas >= 4) and (peso >= 3500) and (peso <= 6000):    
+    print("C")
+elif (rodas >= 4) and (pessoas > 8):   
+    print("D")
+elif (rodas >= 4) and (peso > 6000):
+    print("E")
